2d78193d87b5e591e3dfb820605e67798ce86c27
abdul2112 <abdulkadir.s.salad1@gmail.com>
2021-05-04 16:01:49 +0100

    .

---
+++ src/components/DishComments.jsx
@@ -0,0 +1,40 @@
+import { ListGroup } from 'react-bootstrap'
+import React from 'react';
+
+class DishComments extends React.Component {
+    render() {
+        return (
+            // props.dish is the whole object
+        <div className={`mt-${this.props.marginTop}`}>
+            <h2>Comments for {this.props.dish.name}</h2>
+            <ListGroup>
+            { 
+            this.props.dish.comments.map(c => (
+            <ListGroup.Item key={c.id}>"{c.comment}" from {c.author}</ListGroup.Item>
+            ))
+            }
+    </ListGroup>
+</div>
+        )
+    }
+}
+
+
+
+
+// const DishComments = (props) => (
+//     // props.dish is the whole object
+//     <div className={`mt-${props.marginTop}`}>
+//         <h2>Comments for {props.dish.name}</h2>
+//         <ListGroup>
+//             {
+//                 props.dish.comments.map(c => (
+//                     <ListGroup.Item key={c.id}>"{c.comment}" from {c.author}</ListGroup.Item>
+//                 ))
+//             }
+//         </ListGroup>
+//     </div>
+    
+// )
+
+export default DishComments
